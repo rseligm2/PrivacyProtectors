c72ce7ca592f2f29758faab40216acc2e8c2c7c2
rseligm2 <rseligm2@illinois.edu>
2020-04-22 12:14:24 -0500

    first stuff

---
+++ piechart.js
@@ -0,0 +1,27 @@
+const ctx = document.getElementById('piechart');
+
+const piechart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            data: [20, 35, 15, 10, 20]
+        }],
+
+        // These labels appear in the legend and in the tooltips when hovering different arcs
+        labels: [
+            'Photography',
+            'Fitness',
+            'Insurance',
+            'Beauty & Fashion',
+            'Vacations'
+        ],
+        backgroundColor: [
+            'rgb(0,255,255)',
+            'rgb(25,25,112)',
+            'rgb(44,186,222)',
+            'rgb(0,128,255)',
+            'rgb(0, 153,153)'
+        ]
+    },
+    options: options
+});
